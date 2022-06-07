2bc276681f17f5feb4539ebf4f7be042825252c5
Getúlio Souza <getuliosouza@live.com>
2022-06-07 16:46:22 -0300

    footer finalizado

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 import "./contact.css"
 import {MdOutlineEmail} from "react-icons/md"
 import {AiFillLinkedin} from "react-icons/ai"
-import {ImWhatsapp} from "react-icons/im"
+import { ImWhatsapp } from "react-icons/im"
+import { useRef } from 'react';
+import emailjs from "emailjs-com";
 
 const contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_dzd0l2p', 'template_ix8bjzt', form.current, '8VJrPFt-2tLG8iELe')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+    
+    e.target.reset()
+  };
+
   return (
     <section id='contact'>
       <h5>Get in touch</h5>
@@ -37,7 +53,7 @@ const contact = () => {
 
         </div>
           {/* form */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name="name" placeholder='Your full name' required />
           <input type="email" name='email' placeholder='your e-mail' required />
           <textarea name="message" rows="7" placeholder='your message' required></textarea>
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import "./footer.css"
+import React from "react";
+import "./footer.css";
+import { AiFillGithub } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
 
 const footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        Getúlio Souza
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      {/* social */}
+      <div className="footer__socials">
+        <a href="https://github.com/getulio-souza">
+          <AiFillGithub />
+        </a>
+        <a href="https://www.linkedin.com/in/getulio-souza/">
+          <AiFillLinkedin />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; Getúlio Souza. All Rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
 
-export default footer
+export default footer;
