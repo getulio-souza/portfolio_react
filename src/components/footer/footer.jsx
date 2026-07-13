f69264cdc53d77b039791232e620afa70bdd39f3
Getulio Souza <getuliosouza8@gmail.com>
2026-07-13 16:29:42 -0300

    feature: translations-for-footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -2,46 +2,73 @@ import React from "react";
 import "./footer.css";
 import { AiFillGithub } from "react-icons/ai";
 import { AiFillLinkedin } from "react-icons/ai";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
+  const {t} = useTranslation();
+  
   return (
     <footer>
-      <a href="#" className="footer__logo">
-        Getúlio Souza
-      </a>
-      <ul className="permalinks">
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#experience">Experience</a>
-        </li>
-        <li>
-          <a href="#services">Services</a>
-        </li>
-        <li>
-          <a href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
-      {/* social */}
-      <div className="footer__socials">
-        <a href="https://github.com/getulio-souza">
-          <AiFillGithub />
-        </a>
-        <a href="https://www.linkedin.com/in/getulio-souza/">
-          <AiFillLinkedin />
-        </a>
-      </div>
-      <div className="footer__copyright">
-        <small>&copy; Getúlio Souza. All Rights reserved.</small>
-      </div>
-    </footer>
+  <a href="#" className="footer__logo">
+    Getúlio Souza
+  </a>
+
+  <ul className="permalinks">
+    <li>
+      <a href="#">{t("FOOTER.LINKS.HOME")}</a>
+    </li>
+
+    <li>
+      <a href="#about">{t("FOOTER.LINKS.ABOUT")}</a>
+    </li>
+
+    <li>
+      <a href="#experience">{t("FOOTER.LINKS.EXPERIENCE")}</a>
+    </li>
+
+    <li>
+      <a href="#services">{t("FOOTER.LINKS.SERVICES")}</a>
+    </li>
+
+    <li>
+      <a href="#portfolio">{t("FOOTER.LINKS.PORTFOLIO")}</a>
+    </li>
+
+    <li>
+      <a href="#contact">{t("FOOTER.LINKS.CONTACT")}</a>
+    </li>
+  </ul>
+
+
+  {/* social */}
+  <div className="footer__socials">
+
+    <a 
+      href="https://github.com/getulio-souza"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <AiFillGithub />
+    </a>
+
+    <a 
+      href="https://www.linkedin.com/in/getulio-souza/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <AiFillLinkedin />
+    </a>
+
+  </div>
+
+
+  <div className="footer__copyright">
+    <small>
+      &copy; Getúlio Souza. {t("FOOTER.COPYRIGHT")}
+    </small>
+  </div>
+
+</footer>
   );
 };
 
